147628911f1f8bcc50954f02b9b1d2fbd9e7909c
FedorovSA <itfedorovsa@gmail.com>
2022-05-27 17:35:36 +0300

    ListIterator - update

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -6,25 +6,20 @@ import java.util.function.Predicate;
 public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
+        ListIterator<T> iterator = list.listIterator(index);
             if (iterator.nextIndex() == index) {
                 iterator.add(value);
-                break;
-            }
-            iterator.next();
+
         }
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator(list.size());
-        while (iterator.hasPrevious()) {
-            if (iterator.previousIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.previous();
+        ListIterator<T> iterator = list.listIterator(index);
+        if (iterator.nextIndex() == index) {
+            iterator.next();
+            iterator.add(value);
+
         }
     }
 
