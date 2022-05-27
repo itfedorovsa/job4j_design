679acefd4f7484646a8ce7bbaab45e95d00211d5
FedorovSA <itfedorovsa@gmail.com>
2022-05-27 17:48:23 +0300

    ListIterator - refactoring

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,19 +7,13 @@ public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index);
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-
-        }
+        iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index + 1);
-        if (iterator.previousIndex() == index) {
-            iterator.add(value);
-
-        }
+        iterator.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
