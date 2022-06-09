6eb0aa335b87de3df6458a2215967db1529e7a93
FedorovSA <itfedorovsa@gmail.com>
2022-06-09 20:59:49 +0300

    Server Availability analysis - update

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,37 +1,28 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        Map<String, String> checks = new LinkedHashMap<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+            PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
             reader.lines()
                     .map(m -> m.split(" "))
-                    .forEach(f -> checks.put(f[1], f[0]));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            StringBuilder builder = new StringBuilder();
-            checks.entrySet()
-                    .stream()
-                    .filter(f -> "200".equals(f.getValue()) && builder.length() != 0
-                            || "300".equals(f.getValue()) && builder.length() != 0
-                            || "400".equals(f.getValue()) && builder.length() == 0
-                            || "500".equals(f.getValue()) && builder.length() == 0)
+                    .filter(f -> "200".equals(f[0]) && builder.length() != 0
+                            || "300".equals(f[0]) && builder.length() != 0
+                            || "400".equals(f[0]) && builder.length() == 0
+                            || "500".equals(f[0]) && builder.length() == 0)
                     .forEach(m -> {
-                        if ("400".equals(m.getValue()) || "500".equals(m.getValue())) {
-                            builder.append(m.getKey());
-                            builder.append(";");
-                        } else {
-                            builder.append(m.getKey());
-                            builder.append(";");
-                            writer.println(builder);
-                            builder.delete(0, 18);
-                        }
+                            if ("400".equals(m[0]) || "500".equals(m[0])) {
+                                builder.append(m[1]);
+                                builder.append(";");
+                            } else {
+                                builder.append(m[1]);
+                                builder.append(";");
+                                writer.println(builder);
+                                builder.delete(0, 18);
+                            }
                     });
         } catch (IOException e) {
             e.printStackTrace();
