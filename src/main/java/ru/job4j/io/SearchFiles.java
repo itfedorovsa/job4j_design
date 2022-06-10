985c958e8191aace4e43a2a45be2b62de43ca86a
FedorovSA <itfedorovsa@gmail.com>
2022-06-10 18:50:14 +0300

    Scanning the file system

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    private final Predicate<Path> pred;
+    List<Path> paths = new ArrayList<>();
+
+    public SearchFiles(final Predicate<Path> pred) {
+        this.pred = pred;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (pred.test(file.getFileName())) {
+            paths.add(file.toAbsolutePath());
+        }
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+}
