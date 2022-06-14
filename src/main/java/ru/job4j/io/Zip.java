5f8ca9ee522cda892116c15910b341c7c94d76e2
FedorovSA <itfedorovsa@gmail.com>
2022-06-14 21:30:40 +0300

    Archive the project

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,72 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    public void packFiles(List<Path> sources, File target) {
+            try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+                for (Path source : sources) {
+                    zip.putNextEntry(new ZipEntry(source.toFile().getAbsolutePath()));
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile().getAbsolutePath()))) {
+                        zip.write(out.readAllBytes());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean validation(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        File path = new File(argsName.get("d"));
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Incorrent arguments amount. Required: 3. Use: -d=\"PATH\" -e=.EXTENSION -o=*.zip");
+        }
+        if (!path.exists()) {
+            throw new IllegalArgumentException("Path is not exist!");
+        }
+        if (!path.isDirectory()) {
+            throw new IllegalArgumentException("Path is not a directory!");
+        }
+        if (!argsName.get("e").startsWith(".")) {
+            throw new IllegalArgumentException("Incorrect second parameter form. Use: \"-e=.EXTENSION\"");
+        }
+        if (!argsName.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException("Incorrect third parameter form. Use: \"-o=NAME.zip\"");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+        ArgsName map = ArgsName.of(args);
+        if (zip.validation(args)) {
+            Path path = Paths.get(map.get("d"));
+            List<Path> sources = Search.search(path, p -> !p.toFile().getName().endsWith(map.get("e")));
+            File out = Paths.get(map.get("o")).toFile();
+            zip.packFiles(sources, out);
+        }
+    }
+}
