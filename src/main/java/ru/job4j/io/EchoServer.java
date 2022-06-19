c7413b39a9dcd2c27b8b92780ceba102b39b478f
FedorovSA <itfedorovsa@gmail.com>
2022-06-19 17:26:49 +0300

    Bot (Socket)

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -15,8 +15,12 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String strArgs = in.readLine();
                     System.out.println(strArgs);
-                    if (strArgs.contains("?msg=Bye")) {
+                    if (strArgs.contains("?msg=Exit")) {
                         server.close();
+                    } else if (strArgs.contains("?msg=Hello")) {
+                        out.write("Hello, friend".getBytes());
+                    } else {
+                        out.write("What".getBytes());
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
