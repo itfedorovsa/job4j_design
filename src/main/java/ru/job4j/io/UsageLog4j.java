d784b575d98c876f2190b290c5dbc2af78a63024
FedorovSA <itfedorovsa@gmail.com>
2022-06-19 20:20:33 +0300

    Slf4j - output of variables

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -12,5 +12,32 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Sergey Fedorov";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        String bb = "Byte";
+        byte b = 1;
+        LOG.debug("Type: {}, value: {}", bb, b);
+        String ss = "Short";
+        short s = 2;
+        LOG.debug("Type: {}, value: {}", ss, s);
+        String ii = "Int";
+        int i = 3;
+        LOG.debug("Type: {}, value: {}", ii, i);
+        String ll = "Long";
+        long l = 4;
+        LOG.debug("Type: {}, value: {}", ll, l);
+        String ff = "Float";
+        float f = 5.0f;
+        LOG.debug("Type: {}, value: {}", ff, f);
+        String dd = "Double";
+        double d = 6.0d;
+        LOG.debug("Type: {}, value: {}", dd, d);
+        String cc = "Char";
+        char c = '7';
+        LOG.debug("Type: {}, value: {}", cc, c);
+        String nn = "Boolean";
+        boolean n = true;
+        LOG.debug("Type: {}, value: {}", nn, n);
     }
 }
