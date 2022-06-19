79e992dd4ab2a7a04468f6af76ff027633101ea6
FedorovSA <itfedorovsa@gmail.com>
2022-06-19 17:15:38 +0300

    Socket - update

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,10 +13,12 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String strArgs = in.readLine();
+                    System.out.println(strArgs);
+                    if (strArgs.contains("?msg=Bye")) {
+                        server.close();
+                    }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("?msg=Bye")) {
-                            server.close();
-                        }
                         System.out.println(str);
                     }
                     out.flush();
