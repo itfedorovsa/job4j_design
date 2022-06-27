a062ab4465f320459aca1e411b97c712ae3605ac
FedorovSA <itfedorovsa@gmail.com>
2022-06-27 13:42:11 +0300

    Rules and roles scheme - update

--- data/sql-users-and-roles/insert.sql
+++ data/sql-users-and-roles/insert.sql
@@ -1,4 +1,28 @@
 insert into roles(role) values ('admin');
 insert into roles(role) values ('employee');
+insert into roles(role) values ('guest');
 insert into rules(rule) values ('private');
 insert into rules(rule) values ('public');
+insert into rules(rule) values ('test');
+insert into states(state) values ('open'); 
+insert into states(state) values ('closed'); 
+insert into states(state) values ('in process'); 
+insert into categories(category) values ('important');
+insert into categories(category) values ('common');
+insert into categories(category) values ('critical');
+insert into users(name) values ('Bob');
+insert into users(name) values ('Tom');
+insert into users(name) values ('Jack'); 
+insert into items(item) values ('fix');
+insert into items(item) values ('repair');
+insert into items(item) values ('wish');
+insert into comments(comment) values ('quickly');
+insert into comments(comment) values ('ok');
+insert into comments(comment) values ('maybe');
+insert into attachs(attach) values ('failure.png');
+insert into attachs(attach) values ('CanonMF3010.doc');
+insert into attachs(attach) values ('MyWish.txt');
+insert into roles_rules(name) values('AdminAcc');
+insert into roles_rules(name) values('EmployeeAcc');
+insert into roles_rules(name) values('GuestAcc');
+
