372dc3dc4253148ef385758dcbdca7fedbb615ca
FedorovSA <itfedorovsa@gmail.com>
2022-06-27 13:47:29 +0300

    SQL Select - update

--- data/sql/select.sql
+++ data/sql/select.sql
@@ -5,12 +5,12 @@ create table fauna (
     discovery_date date
 );
 
-insert into fauna(name, avg_age, discovery_date) values ('lake_fish', '900', '01.01.1960');
+insert into fauna(name, avg_age, discovery_date) values ('lake_fish', '900', '1960-02-25');
 insert into fauna(name, avg_age) values ('rabbit', '12000');
-insert into fauna(name, avg_age, discovery_date) values ('frog', '300', '01.01.1948');
-insert into fauna(name, avg_age, discovery_date) values ('bear', '15000', '01.01.1920');
+insert into fauna(name, avg_age, discovery_date) values ('frog', '300', '1948-05-14');
+insert into fauna(name, avg_age, discovery_date) values ('bear', '15000', '1920-09-30');
 
 select * from fauna where name like '%fish%';
 select * from fauna where avg_age >= 10000 and avg_age <= 21000;
 select * from fauna where discovery_date is null;
-select * from fauna where discovery_date < '01.01.1950';
+select * from fauna where discovery_date < '1950-01-01';
