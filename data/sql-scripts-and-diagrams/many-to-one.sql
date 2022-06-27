9fb22e719380adb0bfdf5662bd378b8e508abf8e
FedorovSA <itfedorovsa@gmail.com>
2022-06-27 12:49:34 +0300

    Relationships between tables - update

--- data/sql-scripts-and-diagrams/many-to-one.sql
+++ data/sql-scripts-and-diagrams/many-to-one.sql
@@ -1,11 +1,11 @@
 create table cars(
     id serial primary key,
     brand varchar(255),
-    model varchar(255)
+    model varchar(255),
+    owner_id int references people(id)
 );
 
 create table people(
     id serial primary key,
-    car_id int references cars(id),
     name varchar(255)
 );
