e6a8c222ffefab2e6be558ece62e205af96c16e5
FedorovSA <itfedorovsa@gmail.com>
2022-06-29 13:33:30 +0300

    SQL car storage - update

--- data/sql/car-storage.sql
+++ data/sql/car-storage.sql
@@ -1,50 +1,52 @@
-create table car(
-    id serial primary key,
-    model varchar(255)
-);
-
 create table body(
     id serial primary key,
-    type varchar(255),
-    car_id int references car(id)
+    type varchar(255)
 );
 
 create table engine(
     id serial primary key,
-    volume float,
-    car_id int references car(id)
+    volume float
 );
 
 create table gearbox(
     id serial primary key,
-    gear int,
-    car_id int references car(id)
+    gear int
 );
 
-insert into car (model) values ('corolla'), ('camry'), ('gt86');
-insert into body (type, car_id) values ('sedan', 1), ('sedan', 2), ('hatchback', null), ('coupe', 3);
-insert into engine (volume, car_id) values (1.6, 1), (2.0, null), (2.5, 3);
-insert into gearbox (gear, car_id) values (4, null), (5, 1), (6, 2), (6, null);
+create table car(
+    id serial primary key,
+    model varchar(255),
+    body_id int references body(id),
+    engine_id int references engine(id),
+    gearbox_id int references gearbox(id)
+);
 
-select c.model, b.type body, e.volume engine, g.gear gearbox from car c
+insert into body (type) values ('sedan'), ('hatchback'), ('coupe');
+insert into engine (volume) values (1.6), (2.0), (2.5);
+insert into gearbox (gear) values (4), (5), (6);
+insert into car (model, body_id, engine_id, gearbox_id) values ('corolla', 1, 1, 2);
+insert into car (model, body_id, engine_id, gearbox_id) values ('camry', 1, null, 3);
+insert into car (model, body_id, engine_id, gearbox_id) values ('gt86', 3, 3, null);
+
+select c.model, b.type, e.volume, g.gear from car c
 left join body b
-on b.car_id = c.id
+on c.body_id = b.id
 left join engine e
-on e.car_id = c.id
+on c.engine_id = e.id
 left join gearbox g
-on g.car_id = c.id;
+on c.gearbox_id = g.id;
 
-select b.type from car c
-full join body b
-on b.car_id = c.id
-where c.id is null;
+select b.type from body b
+left join car c
+on c.body_id = b.id
+where c.body_id is null;
 
-select e.volume from car c
-full join engine e 
-on e.car_id = c.id
-where c.id is null;
+select e.volume from engine e
+left join car c
+on c.engine_id = e.id
+where c.engine_id is null;
 
-select g.gear from car c
-full join gearbox g
-on g.car_id = c.id
-where c.id is null;
+select g.gear from gearbox g
+left join car c
+on c.gearbox_id = g.id
+where c.gearbox_id is null;
