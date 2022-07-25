b97b731f1c94d8e1c8bf72c0a7c52bd429c00c7d
FedorovSA <itfedorovsa@gmail.com>
2022-07-25 12:46:12 +0300

    SRP - reports (update)

--- src/main/java/ru/job4j/design/srp/ReportEngineProgrammists.java
+++ src/main/java/ru/job4j/design/srp/ReportEngineProgrammists.java
@@ -19,10 +19,10 @@ public class ReportEngineProgrammists implements Report {
         text.append("<html>")
                 .append(System.lineSeparator())
                 .append("   <head>Name; Hired; Fired; Salary;</head>")
-                .append(System.lineSeparator());
+                .append(System.lineSeparator())
+                .append("   <body>");
         for (Employee employee : store.findBy(filter)) {
-            text.append("   <body>")
-                    .append(employee.getName()).append("; ")
+            text.append(employee.getName()).append("; ")
                     .append(DATE_FORMAT.format(employee.getHired().getTime())).append("; ")
                     .append(DATE_FORMAT.format(employee.getFired().getTime())).append("; ")
                     .append(employee.getSalary()).append("; ");
